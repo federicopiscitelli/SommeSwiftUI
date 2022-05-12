4209db61430bbe9ee599eda2ac678ecda25bbb28
Federico Piscitelli <federico.piscitelli@studenti.unimi.it>
2022-05-12 15:56:42 +0200

    Initial Commit

---
+++ Somme/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Somme
+//
+//  Created by Federico on 12/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Somme/SommeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SommeApp.swift
+//  Somme
+//
+//  Created by Federico on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SommeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
